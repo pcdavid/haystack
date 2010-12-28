f38a80fbd75dca688468b0a831db5b8eba5e1649
Pierre-Charles David <pcdavid@gmail.com>
2010-12-28 17:07:48 +0100

    Fix/update project metadata and re-generate POM.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject impact-report "0.1.0-SNAPSHOT"
-  :description "Generates an HTML report of Trac tickets vs Subversion files impacted."
+(defproject haystack "0.1.0"
+  :description "Cross-references SCM history with issue trackers to
+  find out which source changes correspond to which issue/ticket, and
+  vice versa."
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [hiccup "0.3.1"]]
