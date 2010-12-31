dd8fb1ff32b6294533ba69493c8de0a4564dbe4e
Pierre-Charles David <pcdavid@gmail.com>
2010-12-31 15:16:20 +0100

    Remove dependency to lein-run.
    The built-in "lein run -m" is enough for now.

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [hiccup "0.3.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-		     [lein-run "1.0.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
